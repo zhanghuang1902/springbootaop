e6ae2e41eb17636e422e633304d101fc9dadebdf
zhanghuang <970839007@qq.com>
2020-10-30 15:31:18 +0800

    修复user Test数据

--- aop/src/test/java/com/springboot/aop/AopApplicationTests.java
+++ aop/src/test/java/com/springboot/aop/AopApplicationTests.java
@@ -3,11 +3,29 @@ package com.springboot.aop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 @SpringBootTest
 class AopApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws IOException {
+        FileReader fileReader = null;
+        try {
+            //创建读
+            fileReader = new FileReader("hello.txt");
+            //读文件
+            int len=fileReader.read();
+            while (len!=-1){
+                System.out.println(fileReader.read());
+                len=fileReader.read();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            fileReader.close();
+        }
     }
 
 }
